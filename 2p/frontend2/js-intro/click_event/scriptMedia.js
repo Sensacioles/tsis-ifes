b2ff9fe6877d36a9ad08378e994c2cf43d15015b
Giovanni Sencioles <giovannisencioles@gmail.com>
2024-04-22 17:55:16 -0300

    update event listener: style added

--- 2p/frontend2/js-intro/click_event/scriptMedia.js
+++ 2p/frontend2/js-intro/click_event/scriptMedia.js
@@ -3,5 +3,14 @@ botao.addEventListener('click',function(){
     let n1 = parseInt(document.getElementById('inputUm').value, 10);
     let n2 = parseInt(document.getElementById('inputDois').value, 10);
     let resultado = document.getElementById('resultado');
-    resultado.innerHTML = "A média é igual a " + (n1+n2)/2;
+    let passe = "";
+    let media = (n1+n2)/2;
+    if (media < 60) {
+        resultado.style.color = "red";
+        passe = "reprovado :(";
+    } else {
+        resultado.style.color = "lightgreen";
+        passe = "aprovado! :D";
+    }
+    resultado.innerHTML = "A média é igual a " + (n1+n2)/2 + ". " + "Você está " + passe;
 });
