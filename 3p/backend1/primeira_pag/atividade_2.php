886cde8719f0884a215e2c61b1c169acb8e59f26
Giovanni Sencioles <giovannisencioles@gmail.com>
2024-08-24 07:09:10 -0300

    create backend1 folder and add first php page file

---
+++ 3p/backend1/primeira_pag/atividade_2.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Primeira página em PHP</title>
+</head>
+<body>
+    <?php
+        $image='<img src="https://picsum.photos/500/600"/>';
+        $name='Giovanni';
+        echo $image;
+        echo "<h1> Olá mundo!</h1>", "<h3>Me chamo $name e essa é minha primeira página em PHP</h3>";
+    ?>
+</body>
+</html>
